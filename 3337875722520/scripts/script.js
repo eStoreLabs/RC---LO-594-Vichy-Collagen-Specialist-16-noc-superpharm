fd7933cd41ca9240722b70fe790b0ba884f06659
Jakub Marzewski <109849381+Jakub-WebDev@users.noreply.github.com>
2025-08-10 22:34:28 +0200

    Add files via upload

---
+++ 3337875722520/scripts/script.js
@@ -0,0 +1,24 @@
+const accTab = document.querySelectorAll('.acc-tab');
+
+for (let i = 0; i < accTab.length; i++) {
+  accTab[i].addEventListener('click', function () {
+    this.classList.add('active');
+    let accTabContent = this.nextElementSibling;
+
+    const hideContent = () => {
+      accTabContent.style.maxHeight = null;
+      this.classList.remove('active');
+    };
+
+    const showContent = () => {
+      accTabContent.style.maxHeight = accTabContent.scrollHeight + 'px';
+      this.classList.add('active');
+    };
+
+    if (accTabContent.style.maxHeight) {
+      hideContent();
+    } else {
+      showContent();
+    }
+  });
+}

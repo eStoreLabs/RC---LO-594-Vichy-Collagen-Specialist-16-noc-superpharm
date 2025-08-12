54b61bb90e55f16fe9f1969a99854513bb56c63c
Jakub Marzewski <109849381+Jakub-WebDev@users.noreply.github.com>
2025-08-12 22:42:34 +0200

    Update script.js

--- 3337875722520/scripts/script.js
+++ 3337875722520/scripts/script.js
@@ -1,24 +1,27 @@
-const accTab = document.querySelectorAll('.acc-tab');
+(() => {
+  const accTab = document.querySelectorAll("#es-rc #es-rc-content .es-acc-tab");
 
-for (let i = 0; i < accTab.length; i++) {
-  accTab[i].addEventListener('click', function () {
-    this.classList.add('active');
-    let accTabContent = this.nextElementSibling;
+  for (let i = 0; i < accTab.length; i++) {
+    accTab[i].addEventListener("click", function () {
+      this.classList.add("es-acc-active");
+      let accTabContent = this.nextElementSibling;
 
-    const hideContent = () => {
-      accTabContent.style.maxHeight = null;
-      this.classList.remove('active');
-    };
+      const hideContent = () => {
+        accTabContent.style.maxHeight = null;
+        this.classList.remove("es-acc-active");
+      };
 
-    const showContent = () => {
-      accTabContent.style.maxHeight = accTabContent.scrollHeight + 'px';
-      this.classList.add('active');
-    };
+      const showContent = () => {
+        accTabContent.style.maxHeight = accTabContent.scrollHeight + "px";
+        this.classList.add("es-acc-active");
+      };
 
-    if (accTabContent.style.maxHeight) {
-      hideContent();
-    } else {
-      showContent();
-    }
-  });
-}
+      if (accTabContent.style.maxHeight) {
+        hideContent();
+      } else {
+        showContent();
+      }
+    });
+  }
+})();
+
